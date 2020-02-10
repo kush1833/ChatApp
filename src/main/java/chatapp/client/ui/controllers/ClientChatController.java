90fbb527e18046a196facf1b15c457f56e085dc3
divyanshu1610 <divyanshu1610@gmail.com>
2020-02-10 23:08:15 +0530

    server-client v1.0

--- src/main/java/chatapp/client/ui/controllers/ClientChatController.java
+++ src/main/java/chatapp/client/ui/controllers/ClientChatController.java
@@ -1,7 +1,12 @@
 package chatapp.client.ui.controllers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import java.util.Set;
 
@@ -20,66 +25,136 @@ import javafx.stage.Stage;
 
 public class ClientChatController implements Initializable {
 
-
-    @FXML Label labelReceiverList;
-    @FXML Button btnSendMessage;
-    @FXML Button btnSendFile;
-    @FXML TextField textMessage;
-    @FXML ListView<String> listView;
+    @FXML
+    Label labelReceiverList;
+    @FXML
+    Button btnSendMessage;
+    @FXML
+    Button btnSendFile;
+    @FXML
+    TextField textMessage;
+    @FXML
+    ListView<String> listView;
 
     private Set<String> receiversSet;
     private Client client;
+    private Path PATH;
 
-    public void initReceiverSet(Set<String> receiversSet, String userString, String initData){
+    public void initReceiverSet(Set<String> receiversSet, String userString, String initData) {
         this.receiversSet = receiversSet;
-         if(initData != null)
+        if (initData != null)
             listView.getItems().add(initData);
         labelReceiverList.setText(userString);
     }
 
-    public Set<String> getReceiversList(){
+    public Set<String> getReceiversList() {
         return this.receiversSet;
     }
 
-    public void updateMessage(Message message){
+    public void updateMessage(Message message) {
 
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-               String msg = message.getSender()+" : "+message.getData();
-               listView.getItems().add(msg);
-            }     
+                String msg;
+                if (message.containsFile()) {
+                    msg = message.getSender() + " : " + message.getFile().getName();
+                    saveToFile(message.getFile());
+                } else {
+                    msg = message.getSender() + " : " + message.getData();
+                }
+                listView.getItems().add(msg);
+            }
         });
     }
 
-    public void sendMessageHandler(MouseEvent event){
+    private void saveToFile(File file) {
+
+        FileInputStream fileInputStream = null;
+        byte[] bytesArray = null;
+        try {
+
+            bytesArray = new byte[(int) file.length()];
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytesArray);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        initDownloadsFolder();
+        PATH = Paths.get("./" + client.getUsername() + "/" + file.getName());
+        try {
+            Files.write(PATH, bytesArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initDownloadsFolder() {
+        String path = "./" + client.getUsername();
+        Path downDir = Paths.get(path);
+        if (!Files.exists(downDir)) {
+            try {
+                Files.createDirectory(downDir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessageHandler(MouseEvent event) {
         String msg = textMessage.getText().trim();
-        Message message = new Message(client.getUsername(), this.receiversSet,msg);
+        Message message = new Message(client.getUsername(), this.receiversSet, msg);
         client.sendMessage(message);
-      
+
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-               textMessage.setText("");
-               String msg = client.getUsername()+" : "+message.getData();
-               listView.getItems().add(msg);
-            }     
+                textMessage.setText("");
+                String msg = client.getUsername() + " : " + message.getData();
+                listView.getItems().add(msg);
+            }
         });
     }
 
-    public void sendFileHandler(MouseEvent event){
-        
+    public void sendFileHandler(MouseEvent event) {
+
         Stage stage = new Stage();
         stage.setTitle("File Chooser");
         FileChooser fileChooser = new FileChooser();
         stage.show();
-        File file = fileChooser.showOpenDialog(stage); 
+        File file = fileChooser.showOpenDialog(stage);
         System.out.println(file);
+
+        if (file != null) {
+            Message message = new Message(client.getUsername(), this.receiversSet, file);
+            client.sendMessage(message);
+
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    textMessage.setText("");
+                    String msg = client.getUsername() + " : " + message.getFile().getName();
+                    listView.getItems().add(msg);
+                }
+            });
+        }
+        stage.close();
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.client = ClientAppController.getClient();
-       
+
     }
 }
--- src/main/java/chatapp/message/Message.java
+++ src/main/java/chatapp/message/Message.java
@@ -1,5 +1,6 @@
 package chatapp.message;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.Set;
 
@@ -11,12 +12,15 @@ public class Message implements Serializable{
     private Set<String> receiversSet;
     private String receiver; 
     private String data;
+    private File file;
+
 
     public Message(String sender, Set<String> receivers,String data) {
         this.sender = sender;
         this.receiversSet = receivers;
         this.data = data;
         this.receiver = null;
+        this.file = null;
     }
 
     public Message(String sender, String receiver,String data) {
@@ -24,9 +28,15 @@ public class Message implements Serializable{
         this.receiver = receiver;
         this.data = data;
         this.receiversSet = null;
+        this.file = null;
     }
 
-  
+    public Message(String sender, Set<String> receivers, File file){
+        this.sender = sender;
+        this.receiversSet = receivers;
+        this.file = file;
+        this.receiver = null;
+    }
 
     public String getSender(){
         return this.sender;
@@ -42,6 +52,18 @@ public class Message implements Serializable{
 
     public String getData(){
         return this.data;
+    }
+
+    public File getFile(){
+        return this.file;
+    }
+
+
+
+    public boolean containsFile(){
+        if(this.file!=null)
+            return true;
+        return false;
     }
 
     public boolean isMultiReceiver(){
